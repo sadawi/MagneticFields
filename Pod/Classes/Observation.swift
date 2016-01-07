363037ae29ab4c617d0bebc737357a37a29d9960
Sam Williams <samuel.williams@gmail.com>
2016-01-07 02:15:04 -0500

    comments

--- Pod/Classes/Observation.swift
+++ Pod/Classes/Observation.swift
@@ -8,6 +8,16 @@
 
 import Foundation
 
+/**
+ An object that holds a closure that is to be run when a value changes.
+ 
+ Observations are themselves Observable, which means we can chain them:
+    let a = Field<String>()
+    let b = Field<String>()
+    let c = Field<String>()
+ 
+    a --> b --> c
+ */
 public class Observation<T>: Observable {
     public typealias ValueType = T
     
@@ -31,6 +41,9 @@ public class Observation<T>: Observable {
     }
 }
 
+/**
+ A mapping of owner objects to Observations.  Owner references are weak.  Observation references are strong.
+ */
 public class ObservationRegistry<V> {
     var observations:NSMapTable = NSMapTable.weakToStrongObjectsMapTable()
     
